dd6cfcb878dc2eea9f11d5c505b486e06f865267
Teddy Okatch <okatch.tedbicah@gmail.com>
2021-10-10 00:34:49 +0300

    Added students app

---
+++ students/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+class Student(models.Model):
+    admission_number = models.CharField(max_length=50)
+    school_id = models.PositiveIntegerField()
+    address = models.CharField(max_length=200)
+    date_of_birth = models.DateField()
+    telephone = models.CharField(max_length=30)
+    country = models.CharField(max_length=30)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    school_id = models.PositiveIntegerField()
+
+    def __str__(self):
+        return "{} - {}".format(self.school_id, self.admission_number)
