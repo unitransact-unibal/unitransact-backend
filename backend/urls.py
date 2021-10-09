3cfa041d32fcc1e8e3220df0200c07ed2b51ad70
Teddy Okatch <okatch.tedbicah@gmail.com>
2021-10-10 02:55:13 +0300

    added auth routes

--- backend/urls.py
+++ backend/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/students/', include('students.urls'))
+    path('api/auth', include('rest_framework.urls')),
+    path('api/v1/students/', include('students.urls')),
 ]
