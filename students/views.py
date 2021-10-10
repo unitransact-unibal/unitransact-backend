e6e6de68f0e4d544be1a0b4debda9a4d7903f701
Teddy Okatch <okatch.tedbicah@gmail.com>
2021-10-10 03:00:09 +0300

    Restricting views to authed

--- students/views.py
+++ students/views.py
@@ -1,15 +1,16 @@
-from django.db.models import query
-from rest_framework import generics
+from rest_framework import generics, permissions
 
 from students.models import Student
 from students.serializer import StudentSerializer
 
 
 class ListStudent(generics.ListAPIView):
+    permission_classes = (permissions.IsAuthenticated,)
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
 
 
 class DetailStudent(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (permissions.IsAuthenticated,)
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
