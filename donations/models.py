385407ee31edd6849007d31d6654ef5bd9d356cd
Teddy Okatch <okatch.tedbicah@gmail.com>
2021-10-10 16:23:07 +0300

    Added donations wallet model

--- donations/models.py
+++ donations/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+class DonationsWalletTransactions(models.Model):
+    DEBIT = 'db'
+    CREDIT = 'cr'
+    TYPE_CHOICES = [
+        (DEBIT, 'debit'),
+        (CREDIT, 'credit'),
+    ]
+
+    user_id = models.OneToOneField(
+        get_user_model(), on_delete=models.PROTECT)
+    amount = models.DecimalField(max_digits=9, decimal_places=2)
+    transaction_type = models.CharField(
+        max_length=10,
+        choices=TYPE_CHOICES,
+        default=DEBIT
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'donations_wallet_transactions'
