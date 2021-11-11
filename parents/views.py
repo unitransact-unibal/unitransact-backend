eef958ce7f80958bf814ef2e9f44f36aaa1428c4
Teddy Okatch <okatch.tedbicah@gmail.com>
2021-11-11 22:51:35 +0300

    Can filter with id

--- parents/views.py
+++ parents/views.py
@@ -1,11 +1,19 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, filters
+from filters.mixins import (
+    FiltersMixin,
+)
 
 from .models import Parent
 from .serializer import ParentSerializer
 
 
-class ParentViewSet(viewsets.ModelViewSet):
+class ParentViewSet(FiltersMixin, viewsets.ModelViewSet):
     permission_classes = (permissions.IsAuthenticated,)
     queryset = Parent.objects.all()
     serializer_class = ParentSerializer
     lookup_field = "user_id"
+
+    filter_backends = (filters.OrderingFilter,)
+    filter_mappings = {
+        'id': 'id'
+    }
