90a55a430c5c36458f68ad1eca7ef0637a405a93
Teddy Okatch <okatch.tedbicah@gmail.com>
2021-11-12 01:40:45 +0300

    Showing student name

--- students/serializer.py
+++ students/serializer.py
@@ -5,6 +5,9 @@ from .models import Student, StudentParent, StudentWalletTransaction
 
 
 class StudentSerializer(serializers.ModelSerializer):
+    first_name = serializers.ReadOnlyField(source='user_id.first_name')
+    last_name = serializers.ReadOnlyField(source='user_id.last_name')
+
     class Meta:
         model = Student
         fields = (
@@ -17,6 +20,8 @@ class StudentSerializer(serializers.ModelSerializer):
             'country',
             'school_id',
             'user_id',
+            'first_name',
+            'last_name',
             'created_at',
             'updated_at',
         )
@@ -24,6 +29,7 @@ class StudentSerializer(serializers.ModelSerializer):
 
 class StudentParentSerializer(serializers.ModelSerializer):
     parent_id = ParentSerializer(many=False)
+    student_id = StudentSerializer(many=False)
 
     class Meta:
         model = StudentParent
