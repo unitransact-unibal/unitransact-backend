599746b3ee060dd452647eaf5e16bf24b66472bf
Sospeter Birir <sospeterbirir1@gmail.com>
2022-01-29 00:55:14 +0300

    more csrf setting

--- backend/settings.py
+++ backend/settings.py
@@ -209,6 +209,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:9000",
 ]
 CORS_ALLOW_CREDENTIALS = True
+CSRF_TRUSTED_ORIGINS = ["https://unitransact-frontend.vercel.app"]
 
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
